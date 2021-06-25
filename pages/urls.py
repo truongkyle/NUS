1bade417ed82ba4839b59128853a9d77a6ca91e7
JosephTuki <52094477+ttkien3009@users.noreply.github.com>
2021-06-25 14:43:55 +0700

    kien_first_time

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'pages'
 urlpatterns = [
     path('', views.index, name='Pages'),
+    # Common
+    path('login/', views.LoginClass.as_view(), name='login'),
+    path('dashboard/', views.DashboardClass.as_view(), name='dashboard'),
+    # User
+    path('change-password/', views.ChangePasswordClass.as_view(), name='change_password'),
+
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse(' this is pages')
+
+
+# Common
+class LoginClass(View):
+    def get(self, request):
+        return render(request, 'Common/login.html')
+
+
+class DashboardClass(View):
+    def get(self, request):
+        return render(request, 'Common/dashboard.html', {'title': 'Dashboard'})
+
+
+# User
+class ChangePasswordClass(View):
+    def get(self, request):
+        context = {'title': 'Change Password'}
+        return render(request, 'User/change_password.html', context)
