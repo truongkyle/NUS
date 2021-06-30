4f9089367ee51718b9582674ee73412cb01ca22e
JosephTuki <52094477+ttkien3009@users.noreply.github.com>
2021-06-30 09:24:15 +0700

    url and html

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,41 @@ urlpatterns = [
     path('dashboard/', views.DashboardClass.as_view(), name='dashboard'),
     # User
     path('change-password/', views.ChangePasswordClass.as_view(), name='change_password'),
+    path('user-info/', views.UserInfoClass.as_view(), name='user_info'),
+    # Manager
+    path('manager-list/', views.ManagerListClass.as_view(), name='manager_list'),
+    path('add-manager/', views.AddManagerClass.as_view(), name='add_manager'),
+    path('edit-manager/', views.EditManagerClass.as_view(), name='edit_manager'),
+    # Candidate
+    path('candidate-list/', views.CandidateListClass.as_view(), name='candidate_list'),
+    path('add-candidate/', views.AddCandidateClass.as_view(), name='add_candidate'),
+    path('edit-candidate/', views.EditCandidateClass.as_view(), name='edit_candidate'),
+    # Community
+    path('community-list/', views.CommunityListClass.as_view(), name='community_list'),
+    path('add-community/', views.AddCommunityClass.as_view(), name='add_community'),
+    path('edit-community/', views.EditCommunityClass.as_view(), name='edit_community'),
+    # Companion
+    path('companion-list/', views.CompanionListClass.as_view(), name='companion_list'),
+    path('add-companion/', views.AddCompanionClass.as_view(), name='add_companion'),
+    path('edit-companion/', views.EditCompanionClass.as_view(), name='edit_companion'),
+    # Spiritual Guide
+    path('spiritual-guide-list/', views.SpiritualGuideListClass.as_view(), name='spiritual_guide_list'),
+    path('add-spiritual-guide/', views.AddSpiritualGuideClass.as_view(), name='add_spiritual_guide'),
+    path('edit-spiritual-guide/', views.EditSpiritualGuideClass.as_view(), name='edit_spiritual_guide'),
+    # Community Group
+    path('community-group-list/', views.CommunityGroupListClass.as_view(), name='community_group_list'),
+    path('add-community-group/', views.AddCommunityGroupClass.as_view(), name='add_community_group'),
+    path('edit-community-group/', views.EditCommunityGroupClass.as_view(), name='edit_community_group'),
+    # Teacher
+    path('teacher-list/', views.TeacherListClass.as_view(), name='teacher_list'),
+    path('add-teacher/', views.AddTeacherClass.as_view(), name='add_teacher'),
+    path('edit-teacher/', views.EditTeacherClass.as_view(), name='edit_teacher'),
+    # Timetable
+    path('timetable/', views.TimetableListClass.as_view(), name='timetable'),
+    path('add-timetable/', views.AddTimetableClass.as_view(), name='add_timetable'),
+    path('edit-timetable/', views.EditTimetableClass.as_view(), name='edit_timetable'),
+    # Department
+    path('department_list/', views.DepartmentListClass.as_view(), name='department_list'),
+    path('add-department/', views.AddDepartmentClass.as_view(), name='add_department'),
+    path('edit-department/', views.EditDepartmentClass.as_view(), name='edit_department'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -25,3 +25,210 @@ class ChangePasswordClass(View):
     def get(self, request):
         context = {'title': 'Change Password'}
         return render(request, 'User/change_password.html', context)
+
+
+class UserInfoClass(View):
+    def get(self, request):
+        title = 'Personal Info'
+        status_image = 0
+        role_user = 'Candidate'
+        context = {'title': title, 'status_image': status_image, 'role_user': role_user}
+        return render(request, 'User/info_user.html', context)
+
+
+# Manager
+class ManagerListClass(View):
+    def get(self, request):
+        title = 'Managers'
+        context = {'title': title}
+        return render(request, 'Manager/manager_list.html', context)
+
+
+class AddManagerClass(View):
+    def get(self, request):
+        title = 'Add Manager'
+        context = {'title': title}
+        return render(request, 'Manager/add_manager.html', context)
+
+
+class EditManagerClass(View):
+    def get(self, request):
+        title = 'Edit Manager'
+        context = {'title': title}
+        return render(request, 'Manager/edit_manager.html', context)
+
+
+# Candidate
+class CandidateListClass(View):
+    def get(self, request):
+        title = 'Candidates'
+        context = {'title': title}
+        return render(request, 'Candidate/candidate_list.html', context)
+
+
+class AddCandidateClass(View):
+    def get(self, request):
+        title = 'Add Candidate'
+        context = {'title': title}
+        return render(request, 'Candidate/add_candidate.html', context)
+
+
+class EditCandidateClass(View):
+    def get(self, request):
+        title = 'Edit Candidate'
+        context = {'title': title}
+        return render(request, 'Candidate/edit_candidate.html', context)
+
+
+# Community
+class CommunityListClass(View):
+    def get(self, request):
+        title = 'Communities'
+        context = {'title': title}
+        return render(request, 'Community/community_list.html', context)
+
+
+class AddCommunityClass(View):
+    def get(self, request):
+        title = 'Add Community'
+        context = {'title': title}
+        return render(request, 'Community/add_community.html', context)
+
+
+class EditCommunityClass(View):
+    def get(self, request):
+        title = 'Edit Community'
+        context = {'title': title}
+        return render(request, 'Community/edit_community.html', context)
+
+
+# Companion
+class CompanionListClass(View):
+    def get(self, request):
+        title = 'Companions'
+        context = {'title': title}
+        return render(request, 'Companion/companion_list.html', context)
+
+
+class AddCompanionClass(View):
+    def get(self, request):
+        title = 'Add Companion'
+        context = {'title': title}
+        return render(request, 'Companion/add_companion.html', context)
+
+
+class EditCompanionClass(View):
+    def get(self, request):
+        title = 'Edit Companion'
+        context = {'title': title}
+        return render(request, 'Companion/edit_companion.html', context)
+
+
+# Spiritual Guide
+class SpiritualGuideListClass(View):
+    def get(self, request):
+        title = 'Spiritual Guides'
+        context = {'title': title}
+        return render(request, 'SpiritualGuide/spiritual_guide_list.html', context)
+
+
+class AddSpiritualGuideClass(View):
+    def get(self, request):
+        title = 'Add Spiritual Guide'
+        context = {'title': title}
+        return render(request, 'SpiritualGuide/add_spiritual_guide.html', context)
+
+
+class EditSpiritualGuideClass(View):
+    def get(self, request):
+        title = 'Edit Spiritual Guide'
+        context = {'title': title}
+        return render(request, 'SpiritualGuide/edit_spiritual_guide.html', context)
+
+
+# Community Group
+class CommunityGroupListClass(View):
+    def get(self, request):
+        title = 'Community Groups'
+        context = {'title': title}
+        return render(request, 'CommunityGroup/community_group_list.html', context)
+
+
+class AddCommunityGroupClass(View):
+    def get(self, request):
+        title = 'Add Community Group'
+        context = {'title': title}
+        return render(request, 'CommunityGroup/add_community_group.html', context)
+
+
+class EditCommunityGroupClass(View):
+    def get(self, request):
+        title = 'Edit Community Group'
+        context = {'title': title}
+        return render(request, 'CommunityGroup/edit_community_group.html', context)
+
+
+# Teacher
+class TeacherListClass(View):
+    def get(self, request):
+        title = 'Teachers'
+        context = {'title': title}
+        return render(request, 'Teacher/teacher_list.html', context)
+
+
+class AddTeacherClass(View):
+    def get(self, request):
+        title = 'Add Teacher'
+        context = {'title': title}
+        return render(request, 'Teacher/add_teacher.html', context)
+
+
+class EditTeacherClass(View):
+    def get(self, request):
+        title = 'Edit Teacher'
+        context = {'title': title}
+        return render(request, 'Teacher/edit_teacher.html', context)
+
+
+# Timetable
+class TimetableListClass(View):
+    def get(self, request):
+        title = 'Timetable'
+        context = {'title': title}
+        return render(request, 'Timetable/timetable_list.html', context)
+
+
+class AddTimetableClass(View):
+    def get(self, request):
+        title = 'Add Timetable'
+        context = {'title': title}
+        return render(request, 'Timetable/add_timetable.html', context)
+
+
+class EditTimetableClass(View):
+    def get(self, request):
+        title = 'Edit Timetable'
+        context = {'title': title}
+        return render(request, 'Timetable/edit_timetable.html', context)
+
+
+# Department
+class DepartmentListClass(View):
+    def get(self, request):
+        title = 'Departments'
+        context = {'title': title}
+        return render(request, 'Department/department_list.html', context)
+
+
+class AddDepartmentClass(View):
+    def get(self, request):
+        title = 'Add Department'
+        context = {'title': title}
+        return render(request, 'Department/add_department.html', context)
+
+
+class EditDepartmentClass(View):
+    def get(self, request):
+        title = 'Edit Department'
+        context = {'title': title}
+        return render(request, 'Department/edit_department.html', context)
